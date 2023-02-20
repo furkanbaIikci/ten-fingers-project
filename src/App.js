ecccddc3db373b34c9a9006596d8359a32f557ea
Furkan <furkan_blkc@hotmail.com>
2023-02-21 00:27:28 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,14 +3,32 @@ import { useState } from "react";
 import TurkishWords from "./Words/TurkishWords";
 
 function App() {
-  const words = useState(TurkishWords);
+  const words = [...TurkishWords];
+  console.log(words);
 
   return (
     <div>
-      <div className="mx-auto">
-        <div className="">
-          <option selected value="Turkish">Turkish</option>
-          <option value="English">English</option>
+      <div className="mx-auto text-center">
+        <div className="mt-10">
+          <input type="radio" name="language" defaultChecked value="Turkish" />
+          <label style={{ fontSize: "30px" }} for="Turkish">
+            {" "}
+            Türkçe
+          </label>
+          <div />
+          <input type="radio" name="language" value="English" />
+          <label style={{ fontSize: "30px" }} for="English">
+            {" "}
+            English
+          </label>
+        </div>
+      </div>
+
+      <div>
+        <div className="w-2/3 mx-auto mt-16 h-72 bg-slate-300 rounded-lg border-2 border-solid border-black overflow-auto break-words ">
+          {words.map((word, index) => {
+            return <span className="mx-5 text-2xl">{word}</span>;
+          })}
         </div>
       </div>
     </div>
